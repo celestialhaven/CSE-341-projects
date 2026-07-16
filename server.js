ea5b5607fe960d4ca12c431663089ff06720abc6
celestialhaven <jonzmabz04@gmail.com>
2026-07-16 23:34:48 +0800

    added swagger w02

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'Origin, X-Requested-With, Content-Type, Accept, Z-key');
+  res.setHeader('Access-Control-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 app.use(express.json());
 
 
